040bb5f49e1b41688840cef58e7115fd40dc756a
Gabriel <onlinehassle1234@gmail.com>
2025-02-17 07:18:19 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = "8000";
+
+app.get("/", (req, res) => {
+    return res.status(200).json("Welcome to the Home page")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
